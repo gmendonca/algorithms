fed51450bf76739422669954e576cacdfd1804c7
Gustavo Mendonca <guzzgm@gmail.com>
2015-05-25 13:08:15 -0500

    starting

---
+++ bubblesort.cpp
@@ -0,0 +1,42 @@
+#include <cstdlib>
+#include <stdio.h>
+
+
+int main(){
+	int n;
+	printf("Sort a list\nEnter the size of the list:");
+	scanf("%d", &n);
+	
+	if(n <= 0)
+		return 0;
+	
+	int num[n], i, j, tmp;
+	int last = n;
+	int nnew = 0;
+
+	for(i = 0; i < n; i++){
+		num[i] = rand() % n;
+		printf("%d ", num[i]);
+	}
+	printf("\n");
+
+	for(i = 0; i < n; i++){
+		nnew = last;
+		for(j = 0; j < nnew; j++){
+			if(num[j] > num[j+1]){
+				tmp = num[j];
+				num[j] = num[j+1];
+				num[j+1] = tmp;
+				last = j;
+			}
+		}
+	}
+
+	printf("Sorted :\n");
+	for(i = 0; i < n; i++){
+		printf("%d ", num[i]);
+	}
+	printf("\n");
+	return 0;
+
+}
---
+++ findTheLargest.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>
+#include <stdio.h>
+
+
+int findTheLargest(int *num, int n){
+	if (n == 1) {
+        return num[0];
+
+    } else {
+        int max = findTheLargest(num, n-1);
+        //printf("Largest element : %d\n", max);
+        return num[n-1] > max ? num[n-1] : max;
+    }
+}
+
+int findTheLargestRecursive(int *num, int h, int t){
+	if (n == 1) {
+        return num[0];
+
+    } else {
+        int max = findTheLargest(num, n-1);
+        //printf("Largest element : %d\n", max);
+        return num[n-1] > max ? num[n-1] : max;
+    }
+}
+
+int main(){
+	int n;
+	printf("Find the Greatest number in a list\nEnter the size of the list:");
+	scanf("%d", &n);
+	
+	if(n <= 0)
+		return 0;
+	
+	int num[n];
+
+	for(int i = 0; i < n; i++){
+		num[i] = rand() % n;
+		printf("%d ", num[i]);
+	}
+	printf("\n");
+
+	int x = findTheLargest(num, n);
+
+	printf("Largest element : %d\n", x);
+	return 0;
+
+}
